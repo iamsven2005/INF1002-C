63cc02384216aa57e83b7911c5ec965496ff9b7a
Fong-jun <fj.liau@gmail.com>
2026-03-24 19:50:01 +0800

    Commenting

--- query.c
+++ query.c
@@ -1,13 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "headers.h"
 #include "query.h"
-
+/*Search for a record by student ID and display if found*/
 void query(struct Record* records, int records_size, const char* input) {
     if (records_size == 0) {
         printf("No records available to query.\n");
         return;
     }
-
+/*Extract the ID value*/
     char *id_value_start = find_id_value(input);
     if (!id_value_start) {
         printf("Invalid command. Follow the format: QUERY ID=<ID>\n");
