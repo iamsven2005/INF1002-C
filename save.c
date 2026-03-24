f92a7fc65bc101755c7506f4b2b015674adc9e61
Fong-jun <fj.liau@gmail.com>
2026-03-24 14:58:55 +0800

    Minor changes

--- save.c
+++ save.c
@@ -9,7 +9,7 @@ int save(struct Record *records, int total_records, const char *filename) {
     int i;
     for (i = 0; i < total_records; i++)
     {
-        fprintf(file, "%d,%s,%s,%.2f\n", records[i].id, records[i].name, records[i].prog, records[i].marks);
+        fprintf(file, "%d,%s,%s,%.1f\n", records[i].id, records[i].name, records[i].prog, records[i].marks);
     }
     fclose(file);
     printf("The database file \"%s\" is successfully saved.\n", filename);
