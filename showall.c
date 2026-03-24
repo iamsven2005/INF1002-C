0ee26d7d1f784e663338dc2939bff1ec1c492200
Fong-jun <fj.liau@gmail.com>
2026-03-24 14:58:20 +0800

    Minor changes

--- showall.c
+++ showall.c
@@ -19,7 +19,7 @@ int showall(struct Record *records, int total_records) {
     int i;
     for (i = 0; i < total_records; i++)
     {
-        printf("%-10d %-30s %-50s %-6.2f\n", records[i].id, records[i].name, records[i].prog, records[i].marks);
+        printf("%-10d %-30s %-50s %-6.1f\n", records[i].id, records[i].name, records[i].prog, records[i].marks);
     }
     for (int i = 0; i < 100; i++) {
         printf("%s","-");
