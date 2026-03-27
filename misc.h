d115ec13ec7c2977ac893c16674fdf949c25d98b
Fong-jun <fj.liau@gmail.com>
2026-03-27 21:24:53 +0800

    Naming

--- misc.h
+++ misc.h
@@ -10,9 +10,9 @@ char* find_substring_ci(const char *haystack, const char *needle);
 
 // Validation helpers
 int strcasecmp_ci(const char *s1, const char *s2);
-int isValidID(const char *str, int *result);
-int isValidMarks(const char *str, float *result);
-int isValidName(const char *str);
+int isValidID(const char *id_text, int *result);
+int isValidMarks(const char *mark_text, float *result);
+int isValidName(const char *name_text);
 char* find_id_value(const char *input);
 int find_record_index(struct Record *records, int size, int id);
 
