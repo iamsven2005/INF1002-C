c0456e58abc9f39eea8f14476214db6a765fcadc
Fong-jun <fj.liau@gmail.com>
2026-03-27 19:32:03 +0800

    Naming

--- insert.c
+++ insert.c
@@ -11,11 +11,11 @@ int check_record_exists(int id, int *size, struct Record *records) {
 }
 
 /* Insets new record, validate fields and storing the record in memory*/
-bool insert(struct Record data, struct Record *records, int *records_size, char *args) {
-    data.has_id = false;
-    data.has_name = false;
-    data.has_prog = false;
-    data.has_mark = false;
+bool insert(struct Record new_record, struct Record *records, int *records_size, char *args) {
+    new_record.has_id = false;
+    new_record.has_name = false;
+    new_record.has_prog = false;
+    new_record.has_mark = false;
 
     // Method: Getting the position of the "keys" of all attributes
     // then subtract from the next key position to get the value length
@@ -44,23 +44,23 @@ bool insert(struct Record data, struct Record *records, int *records_size, char 
     }
     /*stores detected fields and their positions, easier to identify*/
     struct Key {
-        char *name;
-        char *pos;
+        char *field_name;
+        char *field_position;
     } keys_array[4];
-    keys_array[0].name = "id";;
-    keys_array[0].pos = id_key;
-    keys_array[1].name = "name";
-    keys_array[1].pos = name_key;
-    keys_array[2].name = "programme";
-    keys_array[2].pos = prog_key;
-    keys_array[3].name = "mark";
-    keys_array[3].pos = mark_key;
+    keys_array[0].field_name = "id";;
+    keys_array[0].field_position = id_key;
+    keys_array[1].field_name = "name";
+    keys_array[1].field_position = name_key;
+    keys_array[2].field_name = "programme";
+    keys_array[2].field_position = prog_key;
+    keys_array[3].field_name = "mark";
+    keys_array[3].field_position = mark_key;
     for (int i = 0; i < sizeof(keys_array)/sizeof(keys_array[0]) - 1; i++) {
         for (int j = 0; j < sizeof(keys_array)/sizeof(keys_array[0]) - i - 1; j++) {
-            if (keys_array[j].pos == NULL) continue;
-            if (keys_array[j + 1].pos == NULL) continue;
+            if (keys_array[j].field_position == NULL) continue;
+            if (keys_array[j + 1].field_position == NULL) continue;
 
-            if (keys_array[j].pos > keys_array[j + 1].pos) {
+            if (keys_array[j].field_position > keys_array[j + 1].field_position) {
                 struct Key temp = keys_array[j];
                 keys_array[j] = keys_array[j + 1];
                 keys_array[j + 1] = temp;
@@ -70,15 +70,15 @@ bool insert(struct Record data, struct Record *records, int *records_size, char 
     }
 /*Extracts and validates the value for each provided field.*/
     for (int i = 0; i < (sizeof(keys_array)/sizeof(keys_array[0])); i++) {
-        char *key_name = keys_array[i].name; 
-        char *key_pos = keys_array[i].pos;
+        char *key_name = keys_array[i].field_name; 
+        char *key_pos = keys_array[i].field_position;
         if (key_pos == NULL) continue;
         char *value_pos = key_pos + strlen(key_name) + 1;
         int value_len;
         char *next_key_pos = NULL;
         for (int j = i + 1; j < (sizeof(keys_array)/sizeof(keys_array[0])); j++) {
-            if (keys_array[j].pos != NULL) {
-                next_key_pos = keys_array[j].pos;
+            if (keys_array[j].field_position != NULL) {
+                next_key_pos = keys_array[j].field_position;
                 break;
             }
         }
@@ -115,26 +115,26 @@ bool insert(struct Record data, struct Record *records, int *records_size, char 
                     return true;
                 }
             }
-            data.id = atoi(value);
-            data.has_id = true;
+            new_record.id = atoi(value);
+            new_record.has_id = true;
         }
         else if (strcmp(key_name, "name") == 0) {
             if (strlen(value) > 30) {
                 printf("Name max 30 characters.\n");
                 return true;
             }
-            strncpy(data.name, value, 29);
-            data.name[29] = 0;
-            data.has_name = true;
+            strncpy(new_record.name, value, 29);
+            new_record.name[29] = 0;
+            new_record.has_name = true;
         }
         else if (strcmp(key_name, "programme") == 0) {
             if (strlen(value) > 50) {
                 printf("Programme max 50 characters.\n");
                 return true;
             }
-            strncpy(data.prog, value, 50);
-            data.prog[50] = 0; 
-            data.has_prog = true;
+            strncpy(new_record.prog, value, 50);
+            new_record.prog[50] = 0; 
+            new_record.has_prog = true;
         }
         else if (strcmp(key_name, "mark") == 0) {
             char* endptr;
@@ -148,8 +148,8 @@ bool insert(struct Record data, struct Record *records, int *records_size, char 
                 printf("Marks should be between 0 to 100.\n");
                 return true;
             }
-            data.marks = mark_value;
-            data.has_mark = true;
+            new_record.marks = mark_value;
+            new_record.has_mark = true;
         }
         else {
             printf("Only ID, NAME, PROGRAMME, and MARK fields allowed.\n");
@@ -157,15 +157,15 @@ bool insert(struct Record data, struct Record *records, int *records_size, char 
         }
     }
     /*Rejects insert if Student ID already exists in the current records.*/
-    if ((check_record_exists(data.id, records_size, records) == 1)) {
-        printf("The record with ID=%d already exists.\n", data.id);
+    if ((check_record_exists(new_record.id, records_size, records) == 1)) {
+        printf("The record with ID=%d already exists.\n", new_record.id);
         return true;
     }
     /*IF only ID is given, prompts the user to enter the remaining fields*/
     else {
         if (optional_count == 0) {
-            char name_input[sizeof(data.name)];
-            char prog_input[sizeof(data.prog)];
+            char name_input[sizeof(new_record.name)];
+            char prog_input[sizeof(new_record.prog)];
             char mark_input[32];
             char *endptr = NULL;
 
@@ -197,20 +197,20 @@ bool insert(struct Record data, struct Record *records, int *records_size, char 
                 return true;
             }
             mark_input[strcspn(mark_input, "\n")] = '\0';
-            data.marks = strtof(mark_input, &endptr);
-            if (endptr == mark_input || *endptr != '\0' || data.marks < 0 || data.marks > 100) {
+            new_record.marks = strtof(mark_input, &endptr);
+            if (endptr == mark_input || *endptr != '\0' || new_record.marks < 0 || new_record.marks > 100) {
                 printf("Marks should be between 0 to 100.\n");
                 return true;
             }
 
-            strncpy(data.name, name_input, sizeof(data.name) - 1);
-            data.name[sizeof(data.name) - 1] = '\0';
-            strncpy(data.prog, prog_input, sizeof(data.prog) - 1);
-            data.prog[sizeof(data.prog) - 1] = '\0';
+            strncpy(new_record.name, name_input, sizeof(new_record.name) - 1);
+            new_record.name[sizeof(new_record.name) - 1] = '\0';
+            strncpy(new_record.prog, prog_input, sizeof(new_record.prog) - 1);
+            new_record.prog[sizeof(new_record.prog) - 1] = '\0';
 
-            data.has_name = true;
-            data.has_prog = true;
-            data.has_mark = true;
+            new_record.has_name = true;
+            new_record.has_prog = true;
+            new_record.has_mark = true;
         }
         else if (optional_count != 3) {
             printf("Name, Prog, and Mark fields must be provided together.\n");
@@ -218,11 +218,11 @@ bool insert(struct Record data, struct Record *records, int *records_size, char 
         }
     }
 /*Stores new record and increase the total record count*/
-    records[*records_size] = data; 
+    records[*records_size] = new_record; 
     (*records_size)++;
 
-    if (records[*records_size - 1].id == data.id) {
-        printf("A new record with ID=%d is successfully inserted.\n", data.id);
+    if (records[*records_size - 1].id == new_record.id) {
+        printf("A new record with ID=%d is successfully inserted.\n", new_record.id);
     }
     else {
         perror("Something went wrong while inserting record.\n");
